9701c148a131a61bb0856b6d7bb02f55eb94d346
ehushagen <eric.hushagen@gmail.com>
2021-07-11 17:24:52 -0600

    All 16 buttons set up

--- src/The_Emoterator.cpp
+++ src/The_Emoterator.cpp
@@ -9,6 +9,13 @@
 #define ButtonFiveText "PrawnzoPls "
 #define ButtonSixText "JodiesSmile "
 #define ButtonSevenText "catJAM "
+#define ButtonEightText "JojoPls "
+#define ButtonNineText "prawnzChefsKiss "
+#define ButtonTenText "theche31DerpyBarba "
+#define ButtonElevenText "prawnzGasm "
+#define ButtonTwelveText "prawnzAYAYAShake "
+#define ButtonThirteenText "ConcernFroge "
+#define ButtonFourteenText "hatchlRocky "
 
 //Set the name of the bluetooth keyboard
 BleKeyboard bleKeyboard("The Emoterator");
@@ -20,8 +27,15 @@ ezButton button4(26);
 ezButton button5(33);
 ezButton button6(32);
 ezButton button7(23);
+ezButton button8(22);
+ezButton button9(25);
+ezButton button10(21);
+ezButton button11(18);
+ezButton button12(5);
+ezButton button13(17);
+ezButton button14(16);
 ezButton EnterButton(12);
-// ezButton BackspaceButton(XX);
+ezButton BackspaceButton(19);
 
 void setup() {
   Serial.begin(115200);
@@ -33,7 +47,15 @@ void setup() {
   button5.setDebounceTime(50);
   button6.setDebounceTime(50);
   button7.setDebounceTime(50);
+  button8.setDebounceTime(50);
+  button9.setDebounceTime(50);
+  button10.setDebounceTime(50);
+  button11.setDebounceTime(50);
+  button12.setDebounceTime(50);
+  button13.setDebounceTime(50);
+  button14.setDebounceTime(50);
   EnterButton.setDebounceTime(50);
+  BackspaceButton.setDebounceTime(50);
 
   bleKeyboard.begin();
   bleKeyboard.setBatteryLevel(69);
@@ -47,7 +69,16 @@ void loop() {
   button5.loop();
   button6.loop();
   button7.loop();
+  button8.loop();
+  button9.loop();
+  button10.loop();
+  button11.loop();
+  button12.loop();
+  button13.loop();
+  button14.loop();
   EnterButton.loop();
+  BackspaceButton.loop();
+
   if (bleKeyboard.isConnected()) {
     if (button1.isPressed())
       bleKeyboard.print(ButtonOneText);
@@ -63,9 +94,23 @@ void loop() {
       bleKeyboard.print(ButtonSixText);
     if (button7.isPressed())
       bleKeyboard.print(ButtonSevenText);
+    if (button8.isPressed())
+      bleKeyboard.print(ButtonEightText);
+    if (button9.isPressed())
+      bleKeyboard.print(ButtonNineText);
+    if (button10.isPressed())
+      bleKeyboard.print(ButtonTenText);
+    if (button11.isPressed())
+      bleKeyboard.print(ButtonElevenText);
+    if (button12.isPressed())
+      bleKeyboard.print(ButtonTwelveText);
+    if (button13.isPressed())
+      bleKeyboard.print(ButtonThirteenText);
+    if (button14.isPressed())
+      bleKeyboard.print(ButtonFourteenText);
     if (EnterButton.isPressed())
       bleKeyboard.write(KEY_RETURN);
-//    if (BackspaceButton.isPressed())
-//      bleKeyboard.write(KEY_BACKSPACE);
+    if (BackspaceButton.isPressed())
+      bleKeyboard.write(KEY_BACKSPACE);
   }
 }
